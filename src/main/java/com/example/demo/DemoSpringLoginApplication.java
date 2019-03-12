8f8b786ec14c88df43e93405f34d016a7524c0a3
Redha <benzinered@yahoo.fr>
2019-03-12 17:32:44 +0100

    initial commit

---
+++ src/main/java/com/example/demo/DemoSpringLoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringLoginApplication.class, args);
+	}
+
+}
